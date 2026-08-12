f983818faff59c8f626c4fe9485de592a1e19aac
bhavyapokala2 <bhavyasri2612@gmail.com>
2026-08-12 10:49:45 +0530

    Correct syntax in factorial calculation
    Fix syntax error by adding missing semicolon.

--- Factorial.java
+++ Factorial.java
@@ -2,7 +2,7 @@ public class Factorial {
     public static int calculation(int n){
         int fact=1;
         for(int i=1;i<=n;i++){
-            fact=fact*i
+            fact=fact*i;
         }
         return fact;
     }
