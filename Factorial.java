899bc0b6fc6132bc7a2a9f5ea82751d71c3b6104
bhavyapokala2 <bhavyasri2612@gmail.com>
2026-08-12 10:53:25 +0530

    Fix typo in success message output

--- Factorial.java
+++ Factorial.java
@@ -10,7 +10,7 @@ public class Factorial {
         int n=5;
         int result=calculation(n);
         System.out.println("Factorial of "+n+" is: "+result);
-        System.out.println("Successfull")
+        System.out.println("Successfull");
     }
 
 }
