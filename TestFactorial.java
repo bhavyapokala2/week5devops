e14990dc89c87a40066bfefd810dea6b2e83bf47
bhavyapokala2 <bhavyasri2612@gmail.com>
2026-08-12 09:44:28 +0530

    first commit~

---
+++ TestFactorial.java
@@ -0,0 +1,7 @@
+class TestFactorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int result = Factorial.factorial(n);
+        System.out.println("Factorial of " + n + " is: " + result);
+    }
+}
